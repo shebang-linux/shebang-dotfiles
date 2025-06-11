2a406dd29a900dfbe92d7cd10e618007a7b7c713
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-06-11 18:54:19 +0000

    edit polybar/polybar-scripts/tor.sh

--- polybar/polybar-scripts/tor.sh
+++ polybar/polybar-scripts/tor.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
 _switch() {
-	if [[ "$(ip link | grep 'UP mode' | wc -l)" -gt 0 ]] && [[ "$(curl -s https://check.torproject.org/api/ip | grep -e 'True' -e 'true')" ]]; then
-		doas /root/.local/bin/tor-router stop
-	elif [[ "$(ip link | grep 'UP mode' | wc -l)" -gt 0 ]] && [[ "$(curl -s https://check.torproject.org/api/ip | grep -e 'False' -e 'false')" ]]; then
+	if [[ "$(ip link | grep 'UP mode' | wc -l)" -gt 0 ]] && [[ "$(curl -s https://check.torproject.org/api/ip | grep -e 'False' -e 'false')" ]]; then
 		doas /root/.local/bin/tor-router start
+	else
+		doas /root/.local/bin/tor-router stop
 	fi
 }
 
