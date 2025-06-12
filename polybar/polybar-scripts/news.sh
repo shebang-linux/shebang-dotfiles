2604158f0374b53e54489be3c362590a3fe264b6
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-06-12 07:18:38 +0000

    edit polybar/polybar-scripts/news.sh

--- polybar/polybar-scripts/news.sh
+++ polybar/polybar-scripts/news.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-news_feed=$(curl -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://simplifiedprivacy.com/index.xml)
+[[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]] && news_feed=$(curl -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://simplifiedprivacy.com/index.xml)
 
 counter=1
 
@@ -9,7 +9,7 @@ do
 	news_title=$(echo "$news_feed" | xmlstarlet sel -t -m '//item' -v 'title' -n | awk 'NR=='$counter'')
 	news_link=$(echo "$news_feed" | xmlstarlet sel -t -m '//item' -v 'link' -n | awk 'NR=='$counter'')
 
-	notify=$(notify-send -u critical -i notification-message-im -A "open=Open URL" "$news_title" "Read complete article here") && [[ $notify == "open" ]] && xdg-open "$news_link" &
+	[[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]] && notify=$(notify-send -u critical -i notification-message-im -A "open=Open URL" "$news_title" "Read complete article here") && [[ $notify == "open" ]] && xdg-open "$news_link" &
 
 	echo ""
 
