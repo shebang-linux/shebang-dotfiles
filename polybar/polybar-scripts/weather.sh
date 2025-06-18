c83a6352bc3b71a72a2c661769aa15bdfd35caff
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-06-18 17:52:17 +0000

    edit polybar/polybar-scripts/weather.sh

--- polybar/polybar-scripts/weather.sh
+++ polybar/polybar-scripts/weather.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/sh
 
-[[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]] && CAROUSEL=$(curl -s "https://v3.wttr.in/?format=%s" | head -c5 | tr -d '[:space:]')
-
-if [[ "$CAROUSEL" > 05:30 ]] && [[ "$CAROUSEL" < 17:30 ]]; then
-	pcmanfm-qt --set-wallpaper /usr/share/backgrounds/wallpaper_2.png >/dev/null 2>&1
-else
-	pcmanfm-qt --set-wallpaper /usr/share/backgrounds/wallpaper.png >/dev/null 2>&1
-fi
-
 [[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]] && echo -e $(curl -s "https://v3.wttr.in/?format=%C+%t")
