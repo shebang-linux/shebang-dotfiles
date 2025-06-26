12e41a05c0a5c9fa9bbc72622e07112f559ce98a
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-06-26 13:17:59 +0000

    edit polybar/polybar-scripts/news.sh

--- polybar/polybar-scripts/news.sh
+++ polybar/polybar-scripts/news.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 
 [[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]] && news_feed=$(curl -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://simplifiedprivacy.com/index.xml)
 
