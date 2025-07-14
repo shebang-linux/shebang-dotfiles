86557ee7b45a92182da6105936c2e16b87f345ae
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-07-14 16:13:39 +0000

    edit polybar/polybar-scripts/tor.sh

--- polybar/polybar-scripts/tor.sh
+++ polybar/polybar-scripts/tor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 _switch() {
-	if [[ "$(ip link | grep 'state UP' | wc -l)" -gt 0 ]] && [[ "$(ip link | grep -E 'tun|ppp' | wc -l)" -eq 0 ]] && [[ "$(curl -s https://check.torproject.org/api/ip | grep -E 'False|false')" ]]; then
+	if [[ "$(ip link | grep 'state UP' | wc -l)" -gt 0 ]] && [[ "$(ip link | grep -E 'tun|ppp' | wc -l)" -eq 0 ]] && [[ "$(curl --user-agent 'noleak' -s https://check.torproject.org/api/ip | grep -E 'False|false')" ]]; then
 		doas /root/.local/bin/tor-router start
 	else
 		doas /root/.local/bin/tor-router stop
@@ -13,7 +13,7 @@ case "$1" in
 		_switch
 		;;
 	*)
-		if [[ "$(ip link | grep 'state UP' | wc -l)" -gt 0 ]] && [[ "$(curl -s https://check.torproject.org/api/ip | grep -E 'True|true')" ]]; then
+		if [[ "$(ip link | grep 'state UP' | wc -l)" -gt 0 ]] && [[ "$(curl --user-agent 'noleak' -s https://check.torproject.org/api/ip | grep -E 'True|true')" ]]; then
 			echo -e "\uf023"
 		elif [[ "$(ip link | grep 'state UP' | wc -l)" -gt 0 ]] && [[ "$(ip link | grep -E 'tun|ppp' | wc -l)" -gt 0 ]]; then
 			echo -e "\uf023"
