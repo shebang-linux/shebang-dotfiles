4e6a6659639de3164b1a1a88a23266a5a07e3d7d
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-07-14 16:13:39 +0000

    edit polybar/polybar-scripts/news.sh

--- polybar/polybar-scripts/news.sh
+++ polybar/polybar-scripts/news.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
 if [[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]]; then
-	news_feed=$(curl -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://simplifiedprivacy.com/index.xml)
-	podcast_feed=$(curl -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://podcast.simplifiedprivacy.com/index.xml)
-	video_feed=$(curl -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://video.simplifiedprivacy.com/feed/)
+	news_feed=$(curl --user-agent 'noleak' -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://simplifiedprivacy.com/index.xml)
+	podcast_feed=$(curl --user-agent 'noleak' -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://podcast.simplifiedprivacy.com/index.xml)
+	video_feed=$(curl --user-agent 'noleak' -s -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36' https://video.simplifiedprivacy.com/feed/)
 
 	news_title=$(echo -e "$news_feed" | xmlstarlet sel -t -m '//item' -v 'title' -n | awk 'NR==1')
 	news_link=$(echo -e "$news_feed" | xmlstarlet sel -t -m '//item' -v 'link' -n | awk 'NR==1')
