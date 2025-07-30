8008110ed1100dc097ba8c4cf3421f1483568b82
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-07-30 17:17:27 +0000

    edit polybar/polybar-scripts/notify.sh

--- polybar/polybar-scripts/notify.sh
+++ polybar/polybar-scripts/notify.sh
@@ -2,10 +2,13 @@
 
 case "$1" in
     --toggle)
-        dunstctl set-paused toggle
         if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
+	    dunstctl set-paused true
+
             touch "$HOME/.config/polybar/polybar-scripts/dnd"
         else
+	    dunstctl set-paused false
+
             rm "$HOME/.config/polybar/polybar-scripts/dnd"
         fi
         ;;
@@ -14,15 +17,20 @@ case "$1" in
             if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
                 xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
                 grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" || \
-                dunstctl set-paused toggle
+                dunstctl set-paused false
             fi
+
+            dunstctl close-all
+            dunstctl history-clear
+
             echo -e "\uf0a2"
         else
             if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
                 xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
                 grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" && \
-                dunstctl set-paused toggle
+                dunstctl set-paused true
             fi
+
             echo -e "\uf0f3"
         fi
         ;;
