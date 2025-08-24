99ba45f0bdfd866e7c55a33203d4c55a8321d8bd
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-08-24 09:41:07 +0000

    edit .irssi/scripts/autorun/notify.pl

--- .irssi/scripts/autorun/notify.pl
+++ .irssi/scripts/autorun/notify.pl
@@ -5,6 +5,9 @@ use Irssi;
 sub sig_message_public {
     my ($server, $msg, $nick, $address, $target) = @_;
 
+    $msg =~ s/'/'\\''/g;
+    $msg =~ s/"/"/g;
+
     my $notification_msg = "[$target] $nick: $msg";
 
     system("notify=\$(notify-send -u low -i notification-message-im -A 'open=Open IRC' \'$notification_msg\') && [[ \$notify == 'open' ]] && [[ \$(tmux list-sessions -F '#{session_name} #{session_attached}' | grep '^irc 0\$') ]] && exec xterm -e tmux attach -t irc >/dev/null 2>&1 &");
@@ -12,6 +15,9 @@ sub sig_message_public {
 
 sub sig_message_private {
     my ($server, $msg, $nick, $address) = @_;
+
+    $msg =~ s/'/'\\''/g;
+    $msg =~ s/"/"/g;
 
     system("notify=\$(notify-send -u low -i notification-message-im -A 'open=Open IRC' \'$nick\' \'$msg\') && [[ \$notify == 'open' ]] && [[ \$(tmux list-sessions -F '#{session_name} #{session_attached}' | grep '^irc 0\$') ]] && exec xterm -e tmux attach -t irc >/dev/null 2>&1 &");
 }
