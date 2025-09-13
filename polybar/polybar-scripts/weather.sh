b7395de4f075bc3a337c7e243db1cfc433b2bff8
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-09-13 13:04:52 +0000

    edit polybar/polybar-scripts/weather.sh

--- polybar/polybar-scripts/weather.sh
+++ polybar/polybar-scripts/weather.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-[[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]] && echo -e $(curl --user-agent 'noleak' -s "https://v3.wttr.in/?format=%C+%t")
+[[ $(ip link | grep 'state UP' | wc -l) -gt 0 ]] && echo -e $(curl --user-agent 'noleak' -s "https://v3.wttr.in/?format=%C+%t" | grep -v 'Unknown')
