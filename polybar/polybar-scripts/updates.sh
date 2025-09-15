4362416213e801afea33733179aabb4b46ceeb7f
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-09-15 08:46:32 +0000

    edit polybar/polybar-scripts/updates.sh

--- polybar/polybar-scripts/updates.sh
+++ polybar/polybar-scripts/updates.sh
@@ -11,7 +11,7 @@ updates() {
 }
 
 upgrade() {
-    $(xterm -e 'doas pacman -Syyuu --needed --disable-download-timeout;doas flatpak uninstall --delete-data --unused -y;doas flatpak update -y;doas fwupdmgr refresh --assume-yes --force;doas fwupdmgr update --assume-yes --force;doas killall fwupd';polyrestart)
+    $(xterm -e 'doas pacman -Syy --needed --noconfirm --disable-download-timeout archlinux-keyring artix-keyring;doas pacman -Suu --needed --disable-download-timeout;doas flatpak uninstall --delete-data --unused -y;doas flatpak update -y;doas fwupdmgr refresh --assume-yes --force;doas fwupdmgr update --assume-yes --force;doas killall fwupd';polyrestart)
 }
 
 case "$1" in
