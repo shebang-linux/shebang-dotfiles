62795b5aeac8f124889a6f03c1d2c7eb283c7c5d
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-09-15 19:19:31 +0000

    edit polybar/polybar-scripts/usb.sh

--- polybar/polybar-scripts/usb.sh
+++ polybar/polybar-scripts/usb.sh
@@ -4,6 +4,14 @@ usb_print() {
     devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR)
     output=""
 
+    for unmounted in $(echo -e "$devices" | jq -r '.blockdevices[] | select(.rm == true) | select(.mountpoint == null) | .name'); do
+        unmounted=$(echo -e "$unmounted" | tr -d "[:digit:]")
+        unmounted=$(echo -e "$devices" | jq -r '.blockdevices[] | select(.name == "'"$unmounted"'") | .vendor')
+        unmounted=$(echo -e "$unmounted" | tr -d ' ')
+
+        output="\uf052"
+    done
+
     for mounted in $(echo -e "$devices" | jq -r '.blockdevices[] | select(.rm == true) | select(.mountpoint != null) | .size'); do
 
         output="\uf052"
