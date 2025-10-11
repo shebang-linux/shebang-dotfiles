bd129586d177244998f70a0a5c07e04b226c2d7a
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-10-11 13:47:39 +0000

    edit polybar/polybar-scripts/notify.sh

--- polybar/polybar-scripts/notify.sh
+++ polybar/polybar-scripts/notify.sh
@@ -3,34 +3,29 @@
 case "$1" in
     --toggle)
         if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
-	    dunstctl set-paused true
-
             touch "$HOME/.config/polybar/polybar-scripts/dnd"
+            dunstctl set-paused true
         else
-	    dunstctl set-paused false
-
             rm "$HOME/.config/polybar/polybar-scripts/dnd"
+            dunstctl set-paused false
         fi
         ;;
     *)
         if [ "$(dunstctl is-paused)" = "true" ]; then
             if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
-                xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
-                grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" || \
-                dunstctl set-paused false
+                ( xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
+                  grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" || \
+                  dunstctl set-paused false ) &>/dev/null
             fi
-
             dunstctl close-all
             dunstctl history-clear
-
             echo -e "\uf0a2"
         else
             if [[ ! -e "$HOME/.config/polybar/polybar-scripts/dnd" ]]; then
-                xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
-                grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" && \
-                dunstctl set-paused true
+                ( xdotool getwindowgeometry "$(xdotool getactivewindow)" | \
+                  grep -q "$(xdpyinfo | grep dimensions | awk '{print $2}')" && \
+                  dunstctl set-paused true ) &>/dev/null
             fi
-
             echo -e "\uf0f3"
         fi
         ;;
